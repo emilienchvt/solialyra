8055d4371f3c181bd84c0422ad9c832eda2feb4e
Castagnet Cyril <45956179+lecascyril@users.noreply.github.com>
2020-11-16 18:56:45 +0100

    Update correct.sol

--- audit/correct.sol
+++ audit/correct.sol
@@ -1,52 +1,48 @@
-// indiquer la licence
-pragma solidity ^0.5.12; // utiliser un bon pragma: pragma solidity 0.6.11;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.11;
 
-// on utilise safemath sans l'importer, a ne pas oublier
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Crowdsale {
-   using SafeMath for uint256;
+  using SafeMath for uint256;
 
-   //aucun event , dommage pour lire les actions du contrat
+  address public owner; 
+  address payable public escrow; 
+  uint256 public savedBalance; 
+  mapping (address => uint256) public balances; 
 
-   // tout est public, ce n'est pas optimisé/sécurisé
-   address public owner; // the owner of the contract
-   address public escrow; // wallet to collect raised ETH
-   uint256 public savedBalance = 0; // Total amount raised in ETH
-   mapping (address => uint256) public balances; // Balances in incoming Ether
- 
-   // Initialization
+  // event 
+  event PayEther(address _receiver, uint256 _amount, uint256 _date);
 
-   //ceci devrait mieux etre un constructor
-   function Crowdsale(address _escrow) public{ // _escrow doit etre payable
-       owner = tx.origin; // on utilise plutot msg.sender pour l'owner du contrat
-       // add address of the specific contract
-       escrow = _escrow;
-   }
+
+  constructor (address payable _escrow) public{
+      owner = msg.sender;
+      escrow = _escrow;
+  }
   
    // function to receive ETH
-   function() public { // ça doit etre une fonction payable external plutot que ça
-       balances[msg.sender] = balances[msg.sender].add(msg.value);
-       savedBalance = savedBalance.add(msg.value);
-       escrow.send(msg.value); //preferer transfer pour cath l'erreur plutot que revert
-   }
-  
+  receive() payable external {
+      balances[msg.sender] = balances[msg.sender].add(msg.value);
+      savedBalance = savedBalance.add(msg.value);
+      escrow.transfer(msg.value);
+      emit PayEther(escrow, msg.value, now);
+  }
+
    // refund investisor
-   function withdrawPayments() public{ // doit etre payable
-       address payee = msg.sender;
-       uint256 payment = balances[payee];
-       // verifier que payment non nul 
-       // verifier que le smart contract a assez pour payer? normalement c bon
- 
-        // penser a la réentrance, genre avec un bool d'état ou en mettant sa balance a 0
-       payee.send(payment); // transfer, comme ligne 27
- 
-       savedBalance = savedBalance.sub(payment);
-       balances[payee] = 0;
+  function withdrawPayments() public{
+      address payable payee = msg.sender;
+      uint256 payment = balances[payee];
+      require(payment != 0);
+      require(address(this).balance >= payment);
+    
+      savedBalance = savedBalance.sub(payment);
+      balances[payee] = 0;
+    
+      payee.transfer(payment);
+      emit PayEther(payee, payment, block.timestamp);
+  }
 
-       //tout ceci est bizarre logiquement: si on veut retirer de l'argent, 
-       //il n'y en a pas forcement sur le contrat, puisqu'il est encvoyé directement a l'escrow
-       // faire un pullpayment en laissant l'argent sur le crowdsale marcherait mieux
-       // ou encore faire un retrait de fond vers l'escrow, pour redonner à la personne à refund
+    //ceci est la version sans changement de logique du premier contrat
+    // on continue de vider ce smart contract quand on refund, sans redemander l'argent à l'escrow
 
-   }
 }
